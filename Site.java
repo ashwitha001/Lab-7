e1eaef38c28c7f4d8e36d18f73b4c4e82dc6bbae
Owner <ashwithaala@gmail.com>
2022-11-14 18:03:52 -0500

    Initialized function getBillableAmount, getBaseAmount and getTaxAmount

--- Site.java
+++ Site.java
@@ -3,5 +3,10 @@ public abstract class Site {
     public static double _rate;
     public static double _units;
     public static double TAX_RATE = 1.0;
-    public abstract double getBillableAmount();
+    public double getBillableAmount() {
+        return getBaseAmount() + getTaxAmount();
+    }
+
+    public abstract double getBaseAmount();
+    public abstract double getTaxAmount();
 }
