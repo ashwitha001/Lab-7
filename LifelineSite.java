4e102587c9982b97d34916da81e8f0419667c254
Owner <ashwithaala@gmail.com>
2022-11-14 18:12:26 -0500

    extracting code refactoring... same approach as ResidentialSite java class.

--- LifelineSite.java
+++ LifelineSite.java
@@ -1,10 +1,11 @@
 public class LifelineSite extends Site{
-    private double _units;
-    private double _rate;
+
     @Override
-    public double getBillableAmount(){
-        double base = _units * _rate * 0.5;
-        double tax = base * Site.TAX_RATE * 0.2;
-        return base + tax;
+    public double getBaseAmount(){
+        return _units * _rate * 0.5;
+    }
+    @Override
+    public double getTaxAmount(){
+        return getBaseAmount() * Site.TAX_RATE * 0.2;
     }
 }
