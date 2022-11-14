dda897ee5365626e481f74ca433b97b00a016cd5
Owner <ashwithaala@gmail.com>
2022-11-14 18:02:11 -0500

    Added variables rate and units in Site class

--- Site.java
+++ Site.java
@@ -1,4 +1,7 @@
 public abstract class Site {
+
+    public static double _rate;
+    public static double _units;
     public static double TAX_RATE = 1.0;
     public abstract double getBillableAmount();
 }
