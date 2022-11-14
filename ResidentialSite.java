680d6c107706186000ef94d9e87b96ab0fb995b2
Owner <ashwithaala@gmail.com>
2022-11-14 18:11:26 -0500

    extracting code: replacing the repeating of double base and tax and getBillable Amount, by creating functions getBaseAmount and getTaxAmount.

--- ResidentialSite.java
+++ ResidentialSite.java
@@ -1,10 +1,10 @@
 public class ResidentialSite extends Site{
-    private double _units;
-    private double _rate;
     @Override
-    public double getBillableAmount(){
-        double base = _units * _rate;
-        double tax = base * Site.TAX_RATE;
-        return base + tax;
+    public double getBaseAmount(){
+        return _units * _rate;
+    }
+    @Override
+    public double getTaxAmount(){
+        return getBaseAmount() * Site.TAX_RATE;
     }
 }
