a6c8ab3e8602b54ef50246c70bf0234a6fc0541f
Owner <ashwithaala@gmail.com>
2022-11-14 17:25:00 -0500

    Code prior to refactoring. (changes)

--- LifelineSite.java
+++ LifelineSite.java
@@ -1,8 +1,6 @@
-
 public class LifelineSite extends Site{
     private double _units;
     private double _rate;
-
     @Override
     public double getBillableAmount(){
         double base = _units * _rate * 0.5;
